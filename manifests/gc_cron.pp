294876b236337236e1c9dd035036e3b2ebdd1def
Nick Walker <nick.walker@puppetlabs.com>
2016-11-12 08:25:36 -0800

    Rename module to simply puppetdb_gc
    Prior to this commit, the module was named with pe_ at the front.
    Since the module is generally useful, the pe_ can be removed

--- manifests/gc_cron.pp
+++ manifests/gc_cron.pp
@@ -1,4 +1,4 @@
-define pe_puppetdb_gc::gc_cron (
+define puppetdb_gc::gc_cron (
   Enum['absent', 'present'] $gc_cron_ensure = 'present',
   String                    $puppetdb_host  = '127.0.0.1',
   Integer                   $puppetdb_port  = 8080,
@@ -13,7 +13,7 @@ define pe_puppetdb_gc::gc_cron (
 
   cron { "puppet_db_gc_${title}" :
       ensure  => $gc_cron_ensure,
-      command => epp( 'pe_puppetdb_gc/puppetdb_cmd_curl.epp',
+      command => epp( 'puppetdb_gc/puppetdb_cmd_curl.epp',
                     { puppetdb_host => $puppetdb_host,
                       puppetdb_port => $puppetdb_port,
                       api_command   => $api_command,
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,23 @@
-class pe_puppetdb_gc (
+class puppetdb_gc (
   Enum['absent', 'present'] $puppetdb_gc_cron_ensure = 'present',
 ) {
 
-  pe_puppetdb_gc::gc_cron { 'expire_nodes' :
+  puppetdb_gc::gc_cron { 'expire_nodes' :
     gc_cron_ensure => $puppetdb_gc_cron_ensure,
     cron_minute    => 1,
   }
 
-  pe_puppetdb_gc::gc_cron { 'purge_nodes' :
+  puppetdb_gc::gc_cron { 'purge_nodes' :
     gc_cron_ensure => $puppetdb_gc_cron_ensure,
     cron_minute    => 5,
   }
 
-  pe_puppetdb_gc::gc_cron { 'purge_reports' :
+  puppetdb_gc::gc_cron { 'purge_reports' :
     gc_cron_ensure => $puppetdb_gc_cron_ensure,
     cron_minute    => [15,45],
   }
 
-  pe_puppetdb_gc::gc_cron { 'other' :
+  puppetdb_gc::gc_cron { 'other' :
     gc_cron_ensure => $puppetdb_gc_cron_ensure,
     cron_minute    => 55,
     cron_day       => 20,
