c157a111df9750c79dc7b25fb3af09010a6a1bde
Nick Walker <nick.walker@puppetlabs.com>
2016-11-14 11:30:36 -0800

    Purge reports every 15 minutes instead of every 30
    In order to facilitate purging reports at 0 move expiring nodes to
    minute 3.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@ class puppetdb_gc (
 
   puppetdb_gc::gc_cron { 'expire_nodes' :
     gc_cron_ensure => $puppetdb_gc_cron_ensure,
-    cron_minute    => 1,
+    cron_minute    => 3,
   }
 
   puppetdb_gc::gc_cron { 'purge_nodes' :
@@ -14,7 +14,7 @@ class puppetdb_gc (
 
   puppetdb_gc::gc_cron { 'purge_reports' :
     gc_cron_ensure => $puppetdb_gc_cron_ensure,
-    cron_minute    => [15,45],
+    cron_minute    => [0,15,30,45],
   }
 
   puppetdb_gc::gc_cron { 'other' :
