7510cff6e72f7229fb6f4e82ee1acbf11ff9abfb
Nick Walker <nick.walker@puppetlabs.com>
2016-11-21 11:47:03 -0800

    Refactor init.pp to take the payloads and a schedule as params
    Prior to this commit, all of the cron jobs were hardcoded and
    not configurable.
    
    After this commit, all of the cron jobs are configurable via a
    single parameter that takes payloads and a schedule to run them
    on.  This commit does not change the resulting cron jobs it just
    moves the defaults from hardcoded in the manifest to parameters.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,36 @@
 class puppetdb_gc (
-  Enum['absent', 'present'] $puppetdb_gc_cron_ensure = 'present',
+  Enum['absent', 'present'] $puppetdb_gc_cron_ensure     = 'present',
+  Array[Hash]               $gc_api_payload_and_schedule =
+  [
+    {
+      'gc_api_payload' => 'expire_nodes',
+      'schedule'       => { 'minute' => 3 },
+    },
+    {
+      'gc_api_payload' => 'purge_nodes',
+      'schedule'       => { 'minute' => 5 },
+    },
+    {
+      'gc_api_payload' => 'purge_reports',
+      'schedule'       => { 'minute' => [0,15,30,45], }
+    },
+    {
+      'gc_api_payload' => 'other',
+      'schedule'       => { 'minute'   => 55,
+                            'monthday' => 20, }
+    },
+  ]
 ) {
 
-  puppetdb_gc::gc_cron { 'expire_nodes' :
-    gc_cron_ensure => $puppetdb_gc_cron_ensure,
-    cron_minute    => 3,
-  }
+  $gc_api_payload_and_schedule.each | Hash $gc_api_payload_and_schedule | {
 
-  puppetdb_gc::gc_cron { 'purge_nodes' :
-    gc_cron_ensure => $puppetdb_gc_cron_ensure,
-    cron_minute    => 5,
-  }
+    $gc_api_payload = $gc_api_payload_and_schedule['gc_api_payload']
+    $schedule       = $gc_api_payload_and_schedule['schedule']
 
-  puppetdb_gc::gc_cron { 'purge_reports' :
-    gc_cron_ensure => $puppetdb_gc_cron_ensure,
-    cron_minute    => [0,15,30,45],
+    puppetdb_gc::gc_cron { $gc_api_payload :
+      gc_cron_ensure => $puppetdb_gc_cron_ensure,
+      cron_minute    => $schedule['minute'],
+      cron_day       => $schedule['monthday'],
+    }
   }
-
-  puppetdb_gc::gc_cron { 'other' :
-    gc_cron_ensure => $puppetdb_gc_cron_ensure,
-    cron_minute    => 55,
-    cron_day       => 20,
-  }
-
 }
