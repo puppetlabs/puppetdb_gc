fb839b201e58f7c59b1009a612e5619bbd9d82a9
Nick Walker <nick.walker@puppetlabs.com>
2016-11-22 07:58:48 -0800

    Allow running two GC types at the same time
    Prior to this commit, you could not easily specify that you wanted
    to run two types of GC in the same cron.  For example to run
    expire_nodes and purge_nodes you'd have to set the payload to
    'expire_nodes"],["purge_nodes' because the template added the
    leading and trailing double quotes and brackets.
    
    After this commit, we allow a string or Array[String] for
    api_payload and then format the string so it looks like a json
    array so we no longer need the leading and trailing quotes and
    brackets as they are in the parameter when passed in.

--- manifests/gc_cron.pp
+++ manifests/gc_cron.pp
@@ -1,13 +1,19 @@
 define puppetdb_gc::gc_cron (
-  Enum['absent', 'present'] $gc_cron_ensure = 'present',
-  String                    $puppetdb_host  = '127.0.0.1',
-  Integer                   $puppetdb_port  = 8080,
-  String                    $api_command    = 'clean',
-  Integer                   $api_version    = 1,
-  String                    $api_payload    = $title,
+  Enum['absent', 'present']  $gc_cron_ensure = 'present',
+  String                     $puppetdb_host  = '127.0.0.1',
+  Integer                    $puppetdb_port  = 8080,
+  String                     $api_command    = 'clean',
+  Integer                    $api_version    = 1,
+  Variant[String,Array[String]] $api_payload = $title,
   Puppetdb_gc::Cron_schedule $cron_schedule,
 )
 {
+
+  $_api_payload_formatted = type_of($api_payload) ? {
+    Array[String] => join($api_payload, ' "],[ "'),
+    String        => $api_payload,
+  }
+  $_api_payload = '[ "' + $_api_payload_formatted + '" ]'
 
   cron { "puppet_db_gc_${title}" :
       ensure  => $gc_cron_ensure,
@@ -16,10 +22,10 @@ define puppetdb_gc::gc_cron (
                       puppetdb_port => $puppetdb_port,
                       api_command   => $api_command,
                       api_version   => $api_version,
-                      api_payload   => $api_payload,
+                      api_payload   => $_api_payload,
                     } ),
-      user     => 'root',
-      *        => $cron_schedule,
+      user    => 'root',
+      *       => $cron_schedule,
     }
 
 }
