63a2ec7d85e8e6d54dc91a46cc3ead01b494dd37
Nick Walker <nick.walker@puppetlabs.com>
2016-12-06 15:26:10 -0800

    Make use of SSL configurable
    Prior to this commit, you had to use https to connect to the puppetdb
    API.
    
    After this commit, you can set use_ssl to false and you will get
    defaults to connect to http://127.0.0.1:8080

--- manifests/gc_cron.pp
+++ manifests/gc_cron.pp
@@ -1,7 +1,14 @@
 define puppetdb_gc::gc_cron (
   Enum['absent', 'present'] $gc_cron_ensure = 'present',
-  String                    $puppetdb_host  = $fqdn,
-  Integer                   $puppetdb_port  = 8081,
+  Boolean                   $use_ssl        = true,
+  String                    $puppetdb_host  = $use_ssl ? {
+                                                true  => $fqdn,
+                                                false => '127.0.0.1',
+                                              },
+  Integer                   $puppetdb_port  = $use_ssl ? {
+                                                true  => 8081,
+                                                false => 8080,
+                                              },
   String                    $api_command    = 'clean',
   Integer                   $api_version    = 1,
   String                    $api_payload    = $title,
@@ -15,12 +22,14 @@ define puppetdb_gc::gc_cron (
   cron { "puppet_db_gc_${title}" :
       ensure  => $gc_cron_ensure,
       command => epp( 'puppetdb_gc/puppetdb_cmd_curl.epp',
-                    { puppetdb_host => $puppetdb_host,
+                    { use_ssl       => $use_ssl,
+                      puppetdb_host => $puppetdb_host,
                       puppetdb_port => $puppetdb_port,
                       api_command   => $api_command,
                       api_version   => $api_version,
                       api_payload   => $api_payload,
                       postgresql_host => $postgresql_host,
+
                     } ),
       user     => 'root',
       minute   => $cron_minute,
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,20 @@
 class puppetdb_gc (
   Enum['absent', 'present'] $puppetdb_gc_cron_ensure = 'present',
-  String                    $puppetdb_host           = $fqdn,
-  Integer                   $puppetdb_port           = 8081,
+  Boolean                   $use_ssl                 = true,
+  String                    $puppetdb_host           = $use_ssl ? {
+                                                         true  => $fqdn,
+                                                         false => '127.0.0.1',
+                                                       },
+  Integer                   $puppetdb_port           = $use_ssl ? {
+                                                         true  => 8081,
+                                                         false => 8080,
+                                                       },
   String                    $postgresql_host         = $puppetdb_host,
 ) {
 
   Puppetdb_gc::Gc_cron {
     gc_cron_ensure  => $puppetdb_gc_cron_ensure,
+    use_ssl         => $use_ssl,
     puppetdb_host   => $puppetdb_host,
     puppetdb_port   => $puppetdb_port,
     postgresql_host => $postgresql_host
